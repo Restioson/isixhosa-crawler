62b03d9075f2ecf13bfd6e124bdeb0c1175b5e9d
Restioson <restiosondev@gmail.com>
2022-09-29 10:40:48 +0200

    Add some blocked sites, save one page from them, and retry socket correctly

--- crawler/spiders/isixhosa.py
+++ crawler/spiders/isixhosa.py
@@ -14,7 +14,7 @@ BLOCK_LIST = (
     "wikipedia.org", "wikidata.org", "wikimedia.org", "wiktionary.org", "wikisource.org",
 
     # Navigation elements are translated, and/or content is machine translated
-    "airbnb.co.za", "airbnb.com", "postermywall.com",
+    "airbnb.co.za", "airbnb.com", "postermywall.com", "ubuy.co.zw", "ubuy.za.com",
 
     # Exist solely to provide (often machine translated) dictionaries and translations
     "opentran.net", "glosbe.com", "translated.net", "nativelib.net", "bab.la", "english-dictionary.help",
@@ -27,10 +27,14 @@ BLOCK_LIST = (
     "xperimentalhamid.com", "madreshoy.com", "meteorologiaenred.com", "educationplanetonline.com",
     "solidsmack.com", "builttobrag.com", "descubrir.online", "hablemosdepeces.com", "lavamagazine.com",
     "medfem.co.za", "gm-chk.com", "gamingzeta.com", "ik4.es", "hombresconestilo.com", "cibercactus.com",
-    "paradacreativa.es", "currentschoolnews.com",
+    "paradacreativa.es", "currentschoolnews.com", "yogaoutbreak.com", "cultureoeuvre.com",
 
     # GTranslate comment stripped
-    "worldscholarshub.com", "wikiejemplos.com", "aspergillosis.org", "todoroblox.com"
+    "worldscholarshub.com", "wikiejemplos.com", "aspergillosis.org", "todoroblox.com", "vondt.net", "europeantimes.news",
+    "media-sl.com", "xh.eturbonews.com",
+
+    # Many, many duplicate pages (permalinks), and all the IsiXhosa is not machine readable (images/pdfs)
+    "yumpu.com",
 )
 
 
@@ -41,24 +45,9 @@ def blocked(url: str):
 
 def identify(sock, text):
     text = json.dumps({"text": text, "benchmark": 0}, ensure_ascii=False)
-    response_text = None
-    response = None
-
-    for _ in range(0, 3):
-        try:
-            sock.send(text.encode("utf-8"))
-            response_text = sock.recv(4096).decode("utf-8").replace('confidence:"', 'confidence":')
-            response = json.loads(response_text)
-        except socket.error as err:
-            print(f"err = '{err}'; text =  '{text}'")
-            time.sleep(5)
-            sock = socket.socket()
-            sock.connect((socket.gethostname(), 7770))
-        except json.decoder.JSONDecodeError as err:
-            print(f"err = '{err}'; text = '{text}'; response_text = '{response_text}'")
-            break
-
-    return response
+    sock.send(text.encode("utf-8"))
+    text = sock.recv(4096).decode("utf-8").replace('confidence:"', 'confidence":')
+    return json.loads(text)
 
 
 class IsiXhosaSpider(scrapy.Spider):
@@ -78,7 +67,26 @@ class IsiXhosaSpider(scrapy.Spider):
         return [Request(url, callback=self.parse, priority=1000, dont_filter=False) for url in self.start_urls]
 
     def identify(self, text):
-        return identify(self.sock, text)
+        text = json.dumps({"text": text, "benchmark": 0}, ensure_ascii=False)
+        response_text = None
+        response = None
+
+        for _ in range(0, 3):
+            try:
+                self.sock.send(text.encode("utf-8"))
+                response_text = self.sock.recv(4096).decode("utf-8").replace('confidence:"', 'confidence":')
+                response = json.loads(response_text)
+            except socket.error as err:
+                print(f"err = '{err}'; text =  '{text}'")
+                time.sleep(5)
+                self.sock.close()
+                self.sock = socket.socket()
+                self.sock.connect((socket.gethostname(), 7770))
+            except json.decoder.JSONDecodeError as err:
+                print(f"err = '{err}'; text = '{text}'; response_text = '{response_text}'")
+                break
+
+        return response
 
     def is_isixhosa(self, text, confidence=0.5):
         res = self.identify(text)
@@ -94,7 +102,7 @@ class IsiXhosaSpider(scrapy.Spider):
         # Parse the document and extract sentences for language ID
         soup = BeautifulSoup(response.text, 'lxml')
         text = soup.get_text(" ", strip=True)
-        sentences = [wrapped for sentence in sent_tokenize(text) for wrapped in wrap(sentence, width=500)]
+        sentences = [wrapped for sentence in sent_tokenize(text) for wrapped in wrap(sentence, width=300)]
 
         # No machine-readable text
         if len(sentences) == 0:
@@ -125,29 +133,26 @@ class IsiXhosaSpider(scrapy.Spider):
         def log(action):
             print(f"{action} {response.url}. %/# isiXhosa: {frac_isixhosa * 100:.2f}/{n_isixhosa}. %/# isiZulu: {frac_isizulu * 100:.2f}/{n_isizulu}")
 
-        # Skip page if it is machine translated. Specifically, GTranslate is checked since it is very common
-        g_translate = any(soup.find_all(
-            string=lambda elem: isinstance(elem, Comment) and elem.strip().startswith("delivered by GTranslate")
-        ))
-        if g_translate:
-            # We do save it, though, since it is useful to know what portion of domains are using GTranslate
-            yield {"url": response.url, "g_translate": True}
-            log("Skipping (GTranslate)")
-            return None
-
-        # Also skip a page if it is in the blacklist
-        if blocked(response.url):
-            # We do still save the page - see rationale above for why GTranslate is saved too
-            yield {"url": response.url, "g_translate": True}
-            log("Skipping (Blacklisted)")
-            return None
-
         crawl_all = False
 
         if n_isixhosa >= 5 or frac_isixhosa >= 0.4:
             log("Saving")
             crawl_all = True
             yield {"url": response.url, "frac_isixhosa": frac_isixhosa, "frac_isizulu": frac_isizulu, "content": response.text}
+
+            # Skip page's links if it is machine translated. Specifically, GTranslate is checked since it is very common
+            # We do save it, though, since it is useful to know what portion of domains are using GTranslate
+            g_translate = any(soup.find_all(
+                string=lambda elem: isinstance(elem, Comment) and elem.strip().startswith("delivered by GTranslate")
+            ))
+            if g_translate:
+                log("Skipping (GTranslate)")
+                return None
+
+            # Also skip a page's links if it is in the blacklist
+            if blocked(response.url):
+                log("Skipping (Blacklisted)")
+                return None
         elif n_isixhosa == 0:
             log("No isiXhosa here")
         else:
