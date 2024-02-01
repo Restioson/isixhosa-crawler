bedc6b80fd5abee76e122375e1d192b95b21a345
Restioson <restiosondev@gmail.com>
2024-02-01 11:26:34 +0200

    Count links

--- inspect_out/src/main.rs
+++ inspect_out/src/main.rs
@@ -1,16 +1,20 @@
 #![feature(array_windows)]
 
-use std::borrow::Cow;
 use anyhow::{Context, Result};
 use clap::Parser;
 use gethostname::gethostname;
+use itertools::Itertools;
 use kuchiki::iter::NodeIterator;
 use kuchiki::traits::TendrilSink;
 use pollster::block_on;
 use punkt::params::Standard;
 use punkt::{SentenceTokenizer, TrainingData};
+use rayon::iter::ParallelBridge;
+use rayon::iter::ParallelIterator;
+use regex::Regex;
 use serde::Deserialize;
 use serde::Serialize;
+use std::borrow::Cow;
 use std::cell::RefCell;
 use std::cmp::Reverse;
 use std::collections::HashMap;
@@ -19,12 +23,10 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, BufWriter, Read, Write};
 use std::net::TcpStream;
 use std::sync::{Arc, Mutex};
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::thread;
-use itertools::Itertools;
-use rayon::iter::ParallelBridge;
-use xtra::prelude::*;
-use rayon::iter::ParallelIterator;
 use tldextract::{TldExtractor, TldOption};
+use xtra::prelude::*;
 
 #[derive(Parser)]
 struct Options {
@@ -38,7 +40,6 @@ struct Options {
 }
 
 const LAST_INDEX_FILE: &str = "out/last_index.txt";
-
 
 #[derive(Actor)]
 struct LanguageIdActor {
@@ -75,7 +76,6 @@ impl LanguageIdActor {
         } else {
             IsIsiXhosa::Yes
         }
-
     }
 }
 
@@ -106,13 +106,18 @@ struct GloballyLockedData {
     sites: HashMap<Domain, usize>,
 }
 
-#[derive(Hash, Eq, PartialEq, Debug)]
+#[derive(Hash, Eq, PartialEq, Debug, Clone)]
 struct Domain(String);
 
 impl Domain {
     fn new(url: &str) -> Result<Domain> {
         let res = TldExtractor::new(TldOption::default()).extract(url)?;
-        Ok(Domain([res.subdomain, res.domain, res.suffix].iter().flatten().join(".")))
+        Ok(Domain(
+            [res.subdomain.filter(|s| s != "www"), res.domain, res.suffix]
+                .iter()
+                .flatten()
+                .join("."),
+        ))
     }
 }
 
@@ -136,28 +141,33 @@ fn main() -> Result<()> {
         0
     };
 
-    let (lid, mailbox) = Mailbox::bounded(16);
+    // let lid = {
+    //     let (lid, mailbox) = Mailbox::bounded(16);
+    //
+    //     for i in 0..12 {
+    //         let stream = TcpStream::connect((gethostname().to_str().unwrap(), 7770 + i)).unwrap();
+    //         let (input, output) = (
+    //             BufReader::new(stream.try_clone().unwrap()),
+    //             BufWriter::new(stream),
+    //         );
+    //         let act = LanguageIdActor { input, output };
+    //         let mailbox = mailbox.clone();
+    //         thread::spawn(move || block_on(xtra::run(mailbox, act)));
+    //     }
+    //
+    //     lid
+    // };
 
-    for i in 0..12 {
-        let stream = TcpStream::connect((gethostname().to_str().unwrap(), 7770 + i)).unwrap();
-        let (input, output) = (
-            BufReader::new(stream.try_clone().unwrap()),
-            BufWriter::new(stream),
-        );
-        let act = LanguageIdActor { input, output };
-        let mailbox = mailbox.clone();
-        thread::spawn(move || block_on(xtra::run(mailbox, act)));
-    }
-
-    drop(mailbox);
-
+    let total_outlinks = AtomicUsize::new(0);
+    let outlinks = dashmap::DashMap::new();
     let global_locked_data = Arc::new(Mutex::new(GloballyLockedData::default()));
     let punkt_data = TrainingData::english();
 
     println!("Total words;Total words (deduped sentences);Unique words;Unique trigrams");
 
-    file
-        .lines()
+    let spaces_to_one_re = Regex::new(r"\s+").unwrap();
+
+    file.lines()
         .skip(last)
         .map(Result::unwrap)
         .par_bridge()
@@ -171,159 +181,221 @@ fn main() -> Result<()> {
                         *data.sites.entry(domain).or_insert(0) += 1;
                     }
 
-                    let parser = kuchiki::parse_html().one(content);
-
-                    parser
-                        .inclusive_descendants()
-                        .filter(|node| {
-                            node.as_element().map_or(false, |e| {
-                                matches!(e.name.local.as_ref(), "script" | "style" | "noscript")
-                            })
-                        })
-                        .collect::<Vec<_>>()
-                        .iter()
-                        .for_each(|node| node.detach());
-
-                    let mut content = String::new();
-                    for node in parser.inclusive_descendants().text_nodes() {
-                        let node: &RefCell<String> = &*node;
-                        let node = &node.borrow();
-                        let trimmed = node.trim();
-                        if !trimmed.is_empty() {
-                            content.push_str(trimmed);
-                            content.push(' ');
-                        }
-                    }
-
-                    let xhosa_sentences: Vec<Cow<'_, str>> = SentenceTokenizer::<Standard>::new(&content, &punkt_data)
-                        .into_iter()
-                        .flat_map(|sentence| textwrap::wrap(sentence, 300))
-                        .filter(|sentence| !sentence.is_empty())
-                        .filter(|sentence| {
-                            block_on(lid.send(sentence.to_string())).unwrap() == IsIsiXhosa::Yes
-                        })
-                        .collect();
-
-                    for sentence in xhosa_sentences {
-                        let mut data = global_locked_data.lock().unwrap();
-
-                        let sentence_lower = sentence.to_lowercase();
-                        let sentence_is_dupe = data.sentences.contains_key(&sentence_lower);
-                        *data.sentences.entry(sentence_lower).or_insert(0) += 1;
-
-                        let words: Vec<String> = sentence
-                            .split_whitespace()
-                            .map(|word| word.replace(|c: char| !c.is_ascii_alphabetic(), ""))
-                            .filter(|word| !word.is_empty())
-                            .map(|word| word.to_lowercase())
-                            .collect();
-
-                        for word in words {
-                            *data.vocabulary_with_dupes.entry(word.clone()).or_insert(0) += 1;
-
-                            if !sentence_is_dupe {
-                                *data.vocabulary_without_dupes.entry(word.clone()).or_insert(0) += 1;
-                                data.total_words_deduped_sentences += 1;
-                            }
-
-                            for trigram in word.as_bytes().array_windows::<3>() {
-                                *data.trigrams_with_dupes.entry(*trigram).or_insert(0) += 1;
-
-                                if !sentence_is_dupe {
-                                    *data.trigrams_without_dupes.entry(*trigram).or_insert(0) += 1;
-                                }
-                            }
-
-                            data.total_words += 1;
-
-                            if data.total_words % 100_000 == 0 {
-                                println!(
-                                    "{};{};{};{}",
-                                    data.total_words,
-                                    data.total_words_deduped_sentences,
-                                    data.vocabulary_with_dupes.len(),
-                                    data.trigrams_with_dupes.len(),
-                                );
-                            }
-                        }
-                    }
-
+                    return;
+                    //
+                    // let parser = kuchiki::parse_html().one(content);
+                    //
+                    // parser
+                    //     .inclusive_descendants()
+                    //     .filter(|node| {
+                    //         node.as_element().map_or(false, |e| {
+                    //             matches!(e.name.local.as_ref(), "script" | "style" | "noscript")
+                    //         })
+                    //     })
+                    //     .collect::<Vec<_>>()
+                    //     .iter()
+                    //     .for_each(|node| node.detach());
+                    //
+                    // let mut content = String::new();
+                    // for node in parser.inclusive_descendants().text_nodes() {
+                    //     let node: &RefCell<String> = &*node;
+                    //     let node = &node.borrow();
+                    //     let trimmed = node.trim();
+                    //     if !trimmed.is_empty() {
+                    //         content.push_str(trimmed);
+                    //         content.push(' ');
+                    //     }
+                    // }
+                    //
+                    // let xhosa_sentences: Vec<Cow<'_, str>> =
+                    //     SentenceTokenizer::<Standard>::new(&content, &punkt_data)
+                    //         .into_iter()
+                    //         .flat_map(|sentence| textwrap::wrap(sentence, 300))
+                    //         .filter(|sentence| !sentence.is_empty())
+                    //         .filter(|sentence| {
+                    //             block_on(lid.send(sentence.to_string())).unwrap() == IsIsiXhosa::Yes
+                    //         })
+                    //         .collect();
+                    //
+                    // for sentence in xhosa_sentences {
+                    //     let mut data = global_locked_data.lock().unwrap();
+                    //
+                    //     let sentence = sentence
+                    //         .replace(|c: char| !c.is_ascii_alphabetic(), " ")
+                    //         .to_lowercase();
+                    //     let sentence = spaces_to_one_re.replace_all(&sentence, " ");
+                    //     let sentence_is_dupe = data.sentences.contains_key(sentence.as_ref());
+                    //     *data.sentences.entry(sentence.to_string()).or_insert(0) += 1;
+                    //
+                    //     let words: Vec<&str> = sentence
+                    //         .split_whitespace()
+                    //         .filter(|word| !word.is_empty())
+                    //         .collect();
+                    //     let start_words = data.total_words;
+                    //
+                    //     for word in words {
+                    //         *data
+                    //             .vocabulary_with_dupes
+                    //             .entry(word.to_owned())
+                    //             .or_insert(0) += 1;
+                    //
+                    //         if !sentence_is_dupe {
+                    //             *data
+                    //                 .vocabulary_without_dupes
+                    //                 .entry(word.to_owned())
+                    //                 .or_insert(0) += 1;
+                    //             data.total_words_deduped_sentences += 1;
+                    //         }
+                    //
+                    //         data.total_words += 1;
+                    //     }
+                    //
+                    //     for (a, b, c) in Some(&b'$')
+                    //         .into_iter()
+                    //         .chain(sentence.as_bytes())
+                    //         .chain(Some(&b'.'))
+                    //         .tuple_windows()
+                    //     {
+                    //         let trigram = [*a, *b, *c];
+                    //         *data.trigrams_with_dupes.entry(trigram).or_insert(0) += 1;
+                    //
+                    //         if !sentence_is_dupe {
+                    //             *data.trigrams_without_dupes.entry(trigram).or_insert(0) += 1;
+                    //         }
+                    //     }
+                    //
+                    //     let start_lower_100k = (start_words / 100_000) * 100_000;
+                    //     let end_lower_100k = (data.total_words / 100_000) * 100_000;
+                    //
+                    //     // A 100_000 boundary was crossed, so print stats
+                    //     if end_lower_100k > start_lower_100k {
+                    //         println!(
+                    //             "{};{};{};{}",
+                    //             data.total_words,
+                    //             data.total_words_deduped_sentences,
+                    //             data.vocabulary_with_dupes.len(),
+                    //             data.trigrams_with_dupes.len(),
+                    //         );
+                    //     }
+                    // }
                 }
-                Entry::Link { .. } | Entry::SkippedSite { .. } => (),
+                Entry::Link { from, to }  => {
+                    let from = match Domain::new(&from) {
+                        Ok(d) => d,
+                        Err(_) => return,
+                    };
+                    let to = match Domain::new(&to) {
+                        Ok(d) => d,
+                        Err(_) => return,
+                    };
+
+                    total_outlinks.fetch_add(1, Ordering::Relaxed);
+
+                    if from != to {
+                        *outlinks.entry(to).or_insert(0) += 1;
+                    }
+                },
+                Entry::SkippedSite { .. } => (),
             }
         });
-
+    //
     let data = global_locked_data.lock().unwrap();
-    println!("{};{}", data.total_words, data.vocabulary_with_dupes.len());
-
-    println!("Zipf's law (words) with duplicates");
-    println!("\nRank;Word;Frequency");
-
-    let rankings = data
-        .vocabulary_with_dupes
-        .iter()
-        .sorted_by_key(|(_word, frequency)| Reverse(*frequency))
-        .take(2000)
-        .enumerate();
-
-    for (rank, (word, frequency)) in rankings {
-        println!("{rank};{word};{frequency}");
-    }
-
-    println!("Zipf's law (words) without duplicates");
-    println!("\nRank;Word;Frequency");
-
-    let rankings = data
-        .vocabulary_without_dupes
-        .iter()
-        .sorted_by_key(|(_word, frequency)| Reverse(*frequency))
-        .take(2000)
-        .enumerate();
-
-    for (rank, (word, frequency)) in rankings {
-        println!("{rank};{word};{frequency}");
-    }
-
-    println!("Zipf's law (trigrams) with duplicates");
-    println!("\nRank;Word;Frequency");
-
-    let rankings = data
-        .trigrams_with_dupes
-        .iter()
-        .sorted_by_key(|(_trigram, frequency)| Reverse(*frequency))
-        .take(2000)
-        .enumerate();
-
-    for (rank, (trigram, frequency)) in rankings {
-        println!("{};{};{}", rank, std::str::from_utf8(trigram).unwrap(), frequency);
-    }
-
-    println!("Zipf's law (trigrams) without duplicates");
-    println!("\nRank;Word;Frequency");
-
-    let rankings = data
-        .trigrams_without_dupes
-        .iter()
-        .sorted_by_key(|(_trigram, frequency)| Reverse(*frequency))
-        .take(2000)
-        .enumerate();
-
-    for (rank, (trigram, frequency)) in rankings {
-        println!("{};{};{}", rank, std::str::from_utf8(trigram).unwrap(), frequency);
-    }
-
-    println!("\nTotal sentences;Unique sentences");
-    println!(
-        "{};{}",
-        data.sentences.values().sum::<usize>(),
-        data.sentences.len()
-    );
+    // println!("{};{}", data.total_words, data.vocabulary_with_dupes.len());
+    //
+    // println!("Zipf's law (words) with duplicates");
+    // println!("\nRank;Word;Frequency");
+    //
+    // let rankings = data
+    //     .vocabulary_with_dupes
+    //     .iter()
+    //     .sorted_by_key(|(_word, frequency)| Reverse(*frequency))
+    //     .take(2000)
+    //     .enumerate();
+    //
+    // for (rank, (word, frequency)) in rankings {
+    //     println!("{rank};{word};{frequency}");
+    // }
+    //
+    // println!("Zipf's law (words) without duplicates");
+    // println!("\nRank;Word;Frequency");
+    //
+    // let rankings = data
+    //     .vocabulary_without_dupes
+    //     .iter()
+    //     .sorted_by_key(|(_word, frequency)| Reverse(*frequency))
+    //     .take(2000)
+    //     .enumerate();
+    //
+    // for (rank, (word, frequency)) in rankings {
+    //     println!("{rank};{word};{frequency}");
+    // }
+    //
+    // println!("Zipf's law (trigrams) with duplicates");
+    // println!("\nRank;Word;Frequency");
+    //
+    // let rankings = data
+    //     .trigrams_with_dupes
+    //     .iter()
+    //     .sorted_by_key(|(_trigram, frequency)| Reverse(*frequency))
+    //     .take(2000)
+    //     .enumerate();
+    //
+    // for (rank, (trigram, frequency)) in rankings {
+    //     println!(
+    //         "{};{};{}",
+    //         rank,
+    //         std::str::from_utf8(trigram).unwrap(),
+    //         frequency
+    //     );
+    // }
+    //
+    // println!("Zipf's law (trigrams) without duplicates");
+    // println!("\nRank;Word;Frequency");
+    //
+    // let rankings = data
+    //     .trigrams_without_dupes
+    //     .iter()
+    //     .sorted_by_key(|(_trigram, frequency)| Reverse(*frequency))
+    //     .take(2000)
+    //     .enumerate();
+    //
+    // for (rank, (trigram, frequency)) in rankings {
+    //     println!(
+    //         "{};{};{}",
+    //         rank,
+    //         std::str::from_utf8(trigram).unwrap(),
+    //         frequency
+    //     );
+    // }
+    //
+    // println!("\nTotal sentences;Unique sentences");
+    // println!(
+    //     "{};{}",
+    //     data.sentences.values().sum::<usize>(),
+    //     data.sentences.len()
+    // );
 
     println!("\nSites");
     println!("Domain;Pages crawled");
-    for (domain, pages) in data.sites.iter().sorted_by_key(|(_site, pages)| Reverse(*pages)) {
+    for (domain, pages) in data
+        .sites
+        .iter()
+        .sorted_by_key(|(_site, pages)| Reverse(*pages))
+    {
         println!("{domain};{pages}")
     }
+
+    println!("\nOutlinks");
+    println!("Domain;Links from other domains");
+    for entry in outlinks
+        .iter()
+        .sorted_by_key(|entry| Reverse(*entry.value()))
+    {
+        println!("{};{}", entry.key(), entry.value())
+    }
+
+    println!("\nTotal links; Total outlinks");
+    println!("{};{}", total_outlinks.load(Ordering::SeqCst), outlinks.iter().map(|e| *e.value()).sum::<usize>());
 
     Ok(())
 }
